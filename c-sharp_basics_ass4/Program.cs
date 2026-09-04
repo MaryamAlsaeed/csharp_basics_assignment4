f7ed9d562438f35931e2ccbd03314b64e01d7551
Maryam <mariamsaeedalshamy@gmail.com>
2026-09-04 03:46:36 +0300

    Question 5:

--- c-sharp_basics_ass4/Program.cs
+++ c-sharp_basics_ass4/Program.cs
@@ -2,14 +2,17 @@
 {
     internal class Program
     {
-        public static void PrintBookTitle(string title) //q4
+        public static void AddBonusPages(int pages) //q5
         {
-            Console.WriteLine($"Book Title: {title}");
+            pages += 50;
         }
         static void Main(string[] args)
         {
-            //Question 4:
-            PrintBookTitle("Clean Code");
+            //Question 5:
+            int pages = 400;
+            AddBonusPages(pages);
+            Console.WriteLine(pages);
+            //it outputs 400 bec pages are value type so it just takes a copy of the main value
             Console.WriteLine();
 
             //--------------------------------
