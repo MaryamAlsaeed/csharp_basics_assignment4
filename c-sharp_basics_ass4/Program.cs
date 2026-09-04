1d66d294a3061064512308cd6ba59909211b7c61
Maryam <mariamsaeedalshamy@gmail.com>
2026-09-04 03:36:32 +0300

    Add project files.

---
+++ c-sharp_basics_ass4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace c_sharp_basics_ass4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
